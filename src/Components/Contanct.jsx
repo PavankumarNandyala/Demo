5cdb090da0ccf51fcb279bf61f4b580894dae464
Pàvâń kúmãr Reddÿ <pavankumarreddy@Mac.lan>
2025-05-12 22:39:48 +0100

    remove file

--- src/Components/Contanct.jsx
+++ src/Components/Contanct.jsx
@@ -4,7 +4,6 @@ function Contact() {
   return (
     <div>
       <h1>Contact page</h1>
-      <p>8050332262</p>
     </div>
   )
 }
---
+++ src/Components/Remove.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Remove() {
+  return (
+    <div>
+      <h1>Remove</h1>
+    </div>
+  )
+}
+
+export default Remove
